47463c5164136b808fbac8ba6005eabb11d29f47
JuraRusan <76946109+JuraRusan@users.noreply.github.com>
2021-02-19 11:18:43 +0200

    Add files via upload

---
+++ js/navigation.js
@@ -0,0 +1,11 @@
+function toggle (){
+    var sec = document.getElementById('sec')
+    var nav = document.getElementById('navigation')
+    var con = document.getElementById('con')
+    sec.classList.toggle('active')
+    nav.classList.toggle('active')
+    con.classList.toggle('active')
+}
+
+
+
